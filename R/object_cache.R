93ca74842730fcb652fe24ab62f78c7b53260472
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-04-09 09:14:35 +1000

    Reference counting for the object cache
    Each label that points at a real object increments a counter
    against the real object - when that hits zero the object is removed.

--- R/object_cache.R
+++ R/object_cache.R
@@ -1,47 +1,78 @@
 ## Content-addressable get/set.  Could be generally useful.
+##
+## Another way of running this would be to not do the data expiry and
+## keep things around until this dies.
 .R6_object_cache <- R6::R6Class(
   "object_cache",
 
   public=list(
     con=NULL,
-    key=NULL,
-    key_hash=NULL,
-    cache=NULL,
+    keys=NULL,
+    envir=NULL,
 
     initialize=function(con, key) {
-      self$con      <- redis_connection(con)
-      self$key      <- key
-      self$key_hash <- paste0(key, ":hash")
-      self$cache    <- new.env(parent=emptyenv())
+      self$con   <- redis_connection(con)
+      self$keys  <- object_cache_keys(key)
+      self$envir <- new.env(parent=emptyenv())
     },
 
     list=function(con) {
-      as.character(self$con$hkeys(self$key_hash))
+      as.character(self$con$hkeys(self$keys$hash))
     },
 
-    set=function(name, value) {
+    drop=function(names) {
+      con <- self$con
+      keys <- self$keys
+      hashes <- as.character(con$HMGET(keys$hash, names))
+      con$HDEL(keys$hash, names)
+      for (h in hashes) {
+        n <- con$HINCRBY(keys$count, h, -1)
+        if (n <= 0L) {
+          con$HDEL(keys$count, h)
+          con$HDEL(keys$data,  h)
+          rm(list=h, envir=self$envir)
+        }
+      }
+    },
+
+    set=function(name, value, store_in_envir=TRUE) {
       str <- object_to_string(value)
       hash <- hash_string(str)
-      self$con$HSET(self$key_hash, name, hash)
-      self$con$HSET(self$key,      hash, str)
-      assign(hash, value, self$cache)
+      self$con$HSET(self$keys$hash, name, hash)
+      self$con$HINCRBY(self$keys$count, hash, 1)
+      if (self$con$HEXISTS(self$keys$data, hash) == 0L) {
+        self$con$HSET(self$keys$data, hash, str)
+        ## this must also not exist then.
+        if (store_in_envir) {
+          assign(hash, value, self$envir)
+        }
+      }
     },
 
     get=function(name) {
-      hash <- self$con$HGET(self$key_hash, name)
-      if (exists(hash, self$cache, inherits=FALSE)) {
-        get(hash, self$cache, inherits=FALSE)
+      hash <- self$con$HGET(self$keys$hash, name)
+      if (is.null(hash)) {
+        stop("object not found")
+      }
+      if (exists(hash, self$envir, inherits=FALSE)) {
+        get(hash, self$envir, inherits=FALSE)
       } else {
-        ret <- string_to_object(self$con$HGET(self$key, hash))
-        assign(hash, ret, self$cache)
+        ret <- string_to_object(self$con$HGET(self$keys$data, hash))
+        assign(hash, ret, self$envir)
         ret
       }
     }))
+
+object_cache_keys <- function(key) {
+  list(data  = paste0(key, ":data"),
+       hash  = paste0(key, ":hash"),
+       count =paste0(key, ":count"))
+}
 
 object_cache <- function(con, key) {
   .R6_object_cache$new(con, key)
 }
 
 object_cache_cleanup <- function(con, key) {
-  con$DEL(c(key, paste0(key, ":hash")))
+  con$DEL(as.character(object_cache_keys(key)))
 }
--- tests/testthat/test-object-cache.R
+++ tests/testthat/test-object-cache.R
@@ -12,12 +12,41 @@ test_that("object_cache", {
   val <- 1:10
   obj$set("foo", val)
   expect_that(obj$get("foo"), equals(val))
+  hash <- con$HGET(obj$keys$hash, "foo")
+  expect_that(hash, equals(hash_string(object_to_string(val))))
+  expect_that(con$HGET(obj$keys$count, hash), equals("1"))
+  expect_that(con$HGET(obj$keys$data,  hash),
+              equals(object_to_string(val)))
+
   rm(obj)
 
   obj2 <- object_cache(NULL, "tmp:objects")
-  expect_that(ls(obj2$cache), equals(character(0)))
+  expect_that(ls(obj2$envir), equals(character(0)))
   expect_that(obj2$get("foo"), equals(val))
-  expect_that(ls(obj2$cache),
+  expect_that(ls(obj2$envir),
               equals(hash_string(object_to_string(val))))
-  expect_that(obj2$cache[[ls(obj2$cache)]], is_identical_to(val))
+  expect_that(obj2$envir[[ls(obj2$envir)]], is_identical_to(val))
+
+  ## Here is the data hash:
+  expect_that(con$HKEYS(obj2$keys$data),
+              equals(list(hash)))
+
+  ## Now, make a *second* object pointing that the same thing.
+  obj2$set("bar", val)
+
+  ## Nothing extra here:
+  expect_that(con$HKEYS(obj2$keys$data),
+              equals(list(hash)))
+  expect_that(sort(obj2$list()), equals(sort(c("foo", "bar"))))
+  ## Counter reflects that we have two things here:
+  expect_that(con$HGET(obj2$keys$count, hash), equals("2"))
+
+  obj2$drop("foo")
+  expect_that(con$HGET(obj2$keys$count, hash), equals("1"))
+  obj2$drop("bar")
+  expect_that(con$HGET(obj2$keys$count, hash), equals(NULL))
+  expect_that(con$HKEYS(obj2$keys$data), equals(list()))
+  expect_that(con$HLEN(obj2$keys$data),  equals(0L))
+  expect_that(con$HLEN(obj2$keys$count), equals(0L))
+  expect_that(ls(obj2$envir), equals(character(0)))
 })
