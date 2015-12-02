9bfb584f7be355b47c4cfe133beca0a7822df5ac
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-12-02 10:02:45 +0000

    Update to use new storr design.

--- R/object_cache.R
+++ R/object_cache.R
@@ -1,12 +1,12 @@
 ##' @importFrom storr storr
 object_cache <- function(prefix, con) {
-  dr <- storr::driver_redis(prefix, con$config())
+  dr <- storr::driver_redis_api(prefix, con)
   storr::storr(dr)
 }
 
 ## TODO: weirdly this is the same thing as object_cache, but a
 ## different prefix.
 file_cache <- function(prefix, con) {
-  dr <- storr::driver_redis(prefix, con$config())
+  dr <- storr::driver_redis_api(prefix, con)
   storr::storr(dr)
 }
