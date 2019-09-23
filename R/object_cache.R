fec51511e40febee4c58c5398df1d8c62e5a5255
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-09-23 08:43:37 +0100

    Use correct storr/redis driver

--- R/object_cache.R
+++ R/object_cache.R
@@ -1,11 +1,11 @@
 object_cache <- function(prefix, con) {
-  dr <- storr::driver_redis_api(prefix, con)
+  dr <- redux::driver_redis_api(prefix, con)
   storr::storr(dr)
 }
 
 ## TODO: weirdly this is the same thing as object_cache, but a
 ## different prefix.
 file_cache <- function(prefix, con) {
-  dr <- storr::driver_redis_api(prefix, con)
+  dr <- redux::driver_redis_api(prefix, con)
   storr::storr(dr)
 }
